c44b1c31e218241d99acfdd8e3428f670a044080
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2023-02-25 18:02:37 +0530

    firestore ADD,DELETE and UPDATE !!

---
+++ lib/googleSignIn.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuth extends ChangeNotifier {
+  final googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+
+  GoogleSignInAccount get user => _user!;
+
+  Future googleLogIn() async {
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken
+    );
+    await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  Future logout() async{
+    await googleSignIn.disconnect();
+    FirebaseAuth.instance.signOut();
+  }
+}
---
+++ lib/loginscreen.dart
@@ -0,0 +1,20 @@
+import 'package:firebase/googleSignIn.dart';
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            GoogleAuth().googleLogIn();
+          },
+          child: const Text('Continue with Google!'),
+        ),
+      ),
+    );
+  }
+}
